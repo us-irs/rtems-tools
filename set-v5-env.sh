ccf1307fffca799aef00ef106c0ec0cbefbdea99
Robin Mueller <robin.mueller.m@gmail.com>
2021-10-24 11:13:39 +0200

    corrected prefix

--- set-v5-env.sh
+++ set-v5-env.sh
@@ -3,7 +3,7 @@ echo "Entering RTEMS 5 environment"
 
 export RTEMS_VERSION=5
 export PATH="$PATH":"$(pwd)/rtems/$RTEMS_VERSION/bin"
-export RTEMS_TOOLS="$(pwd)/rtems/$RTEMS_VERSION"
+export RTEMS_PREFIX="$(pwd)/rtems/$RTEMS_VERSION"
 
 export CONSOLE_PREFIX="[RTEMS 5]"
 exec /bin/bash
--- set-v6-env.sh
+++ set-v6-env.sh
@@ -3,7 +3,7 @@ echo "Entering RTEMS 6 environment"
 
 export RTEMS_VERSION=6
 export PATH="$PATH":"$(pwd)/rtems/$RTEMS_VERSION/bin"
-export RTEMS_TOOLS="$(pwd)/rtems/$RTEMS_VERSION"
+export RTEMS_PREFIX="$(pwd)/rtems/$RTEMS_VERSION"
 
 export CONSOLE_PREFIX="[RTEMS 6]"
 exec /bin/bash
